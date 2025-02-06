d44e4bfcba04250ef085318da77a3772c68a46f1
Karrass Phiri <phirikarrass@gmail.com>
2025-02-06 05:07:20 +0200

    delete invoice fixed

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -6,7 +6,7 @@ import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+ 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -71,7 +71,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
